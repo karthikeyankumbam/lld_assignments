a7d65ecd7ef717b72bd83f3dab56ed42eb3c2224
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-02-22 00:37:49 +0530

    Solved assignment on DAY 125

---
+++ src/main/java/com/practice/Assignments/lld1/day_125/Introduction_to_threads_3/Client.java
@@ -0,0 +1,19 @@
+package com.practice.Assignments.lld1.day_125.Introduction_to_threads_3;
+
+/**
+ * *Raw Problem**
+ * Write code to achieve the following
+ * A class Client with a main method.
+ * Client class shall take a number n as input.
+ * A class called TableCreator which prints the multiplication table from 1 to 10 for a given number x
+ * x times 1 is x
+ * <p>
+ * ..
+ * <p>
+ * x times 10 is 10x
+ * Client should create a thread for every number between 1 to n, n included and
+ * Pass it to TableCreator to print a multiplication table for that number.
+ * Print format = 2 times 6 is 12
+ */
+public class Client {
+}
---
+++ src/main/java/com/practice/Assignments/lld1/day_125/Introduction_to_threads_3/TableCreator.java
@@ -0,0 +1,4 @@
+package com.practice.Assignments.lld1.day_125.Introduction_to_threads_3;
+
+public class TableCreator {
+}
