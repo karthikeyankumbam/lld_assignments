b6aa9f7e77c50911bc3ee3c7564c63a06349de69
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-02-22 00:32:31 +0530

    Solved assignment on DAY 125

--- src/main/java/com/practice/Assignments/lld1/day_125/Introduction_to_threads_2/Client.java
+++ src/main/java/com/practice/Assignments/lld1/day_125/Introduction_to_threads_2/Client.java
@@ -2,13 +2,25 @@ package com.practice.Assignments.lld1.day_125.Introduction_to_threads_2;
 
 import java.util.Scanner;
 
+/**
+ * **Raw Problem**
+ * Write code to achieve the following
+ * A class Client with a main method.
+ * Client class shall take two numbers as input from the user.
+ * Client class should create a new thread and invoke code in a class called Adder.
+ * Client class shall pass two numbers (taken as input from the user) to the Adder class.
+ * The Adder class should print the sum of two numbers passed to it.
+ * <p>
+ * Important Note - Use the ScalerThread class to create new threads. This is necessary for testing your code.
+ */
 public class Client {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        Adder adder=new Adder(a,b);
-        ScalerThread scalerThread=new ScalerThread(adder);
-        scalerThread.start();;
+        Adder adder = new Adder(a, b);
+        ScalerThread scalerThread = new ScalerThread(adder);
+        scalerThread.start();
+        ;
     }
 }
