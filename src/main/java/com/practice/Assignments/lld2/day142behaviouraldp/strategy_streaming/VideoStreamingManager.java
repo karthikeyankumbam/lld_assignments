1bb87cdbdb54c2c84cba191e0feb29174c6a6880
karthikeyankumbam <karthikeyankumbam123@gmail.com>
2024-05-22 06:14:09 +0530

    commiting the strategy design pattern

--- src/main/java/com/practice/Assignments/lld2/day142behaviouraldp/strategy_streaming/VideoStreamingManager.java
+++ src/main/java/com/practice/Assignments/lld2/day142behaviouraldp/strategy_streaming/VideoStreamingManager.java
@@ -1,8 +1,8 @@
 package com.practice.Assignments.lld2.day142behaviouraldp.strategy_streaming;
 
 public class VideoStreamingManager {
-    private Video video;
-    private QualityAdjustmentStrategy qualityAdjustmentStrategy;
+    private final Video video;
+    private final QualityAdjustmentStrategy qualityAdjustmentStrategy;
 
     public VideoStreamingManager(Video video,QualityAdjustmentStrategy qualityAdjustmentStrategy) {
         this.video = video;
